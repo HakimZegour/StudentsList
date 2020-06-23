55431e29ea9277f1ca6df05eceab5b0b5ae244dc
HakimZegour <48995516+HakimZegour@users.noreply.github.com>
2020-06-23 19:55:19 +0200

    Initial commit

---
+++ StudentsList/WebForm1.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StudentsList
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataClasses1DataContext dataContext = new DataClasses1DataContext();
+            GridView1.DataSource = from etudiant in dataContext.StudentsTables
+                                   where etudiant.Classe == "troisieme"
+                                   select etudiant;
+            GridView1.DataBind();
+        }
+    }
+}
